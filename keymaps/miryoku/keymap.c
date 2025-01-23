7d66c195b221fb8af6b908c0bc6d1d55e4ddb1e0
Kali <gabriel3047.eufrazio@gmail.com>
2025-01-23 10:49:18 -0300

    change space and tab

--- keymaps/miryoku/keymap.c
+++ keymaps/miryoku/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,          /*||*/  KC_Y,         KC_U,          KC_I,         KC_O,           KC_P,
         LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,          /*||*/  KC_H,         LSFT_T(KC_J),  LCTL_T(KC_K), LALT_T(KC_L),   LGUI_T(KC_SCLN),
         KC_Z,         RALT_T(KC_X), KC_C,         KC_V,         KC_B,          /*||*/  KC_N,         KC_M,          KC_COMM,      RALT_T(KC_DOT), KC_SLSH,
-        XXXXXXX,      XXXXXXX,      LT(3,KC_ESC), LT(1,KC_SPC), LT(2,KC_TAB),  /*||*/  LT(5,KC_ENT), LT(4,KC_BSPC), LT(6,KC_DEL), XXXXXXX,       XXXXXXX
+        XXXXXXX,      XXXXXXX,      LT(3,KC_ESC), LT(2,KC_TAB), LT(1,KC_SPC),  /*||*/  LT(5,KC_ENT), LT(4,KC_BSPC), LT(6,KC_DEL), XXXXXXX,       XXXXXXX
     ),
     [1] = LAYOUT(
         RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   /*||*/  KC_AGIN, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,
